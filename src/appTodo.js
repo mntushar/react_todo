2d3678cbdbf686d3c9b99bb1f2effba1969bd7cd
mntushar <mntushar25@gmail.com>
2021-09-25 10:24:15 +0600

    show todo list

--- src/appTodo.js
+++ src/appTodo.js
@@ -1,8 +1,23 @@
 import { Fragment } from 'react';
 import { Button, InputGroup, } from 'react-bootstrap';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import { gql, useQuery } from '@apollo/client';
+import { ListItemIcon, ListItemText } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
 
 
 function App() {
+  const {loading, error, data} = useQuery(gql`
+    {
+      todoList{
+        id
+        date
+        text
+      }
+    }
+  `);
+  
   return (
     <Fragment className="todoApp container-fluid">
       <h1 className="text-primary text-center pt-3">Todo App</h1>
@@ -12,6 +27,25 @@ function App() {
             <input className="form-control" placeholder="Add Todo"/>
             <Button className="btn btn-primary">Add</Button>
           </InputGroup>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-xl-4 m-auto">
+          <List>
+          {data?.todoList?.map((item, ind)=>(
+            <ListItem button key={ind}>
+              <ListItemIcon>
+                <Avatar>
+                 {ind + 1}
+                </Avatar>
+              </ListItemIcon>
+              <ListItemText primary={item?.text}>
+
+              </ListItemText>
+            </ListItem>
+          ))}
+          </List>
         </div>
       </div>
     </Fragment>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom';
 import './appTodo.scss';
 import './appTodo.css';
 import App from './appTodo';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
+
+
+//Apollo Cline configurations
+const client = new ApolloClient({
+  uri: 'http://127.0.0.1:8000/graphql/',
+  cache: new InMemoryCache()
+});
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
